1f4e44dd7c954debd748ab2aeaae1ec33ecfe180
Keith Kurak <keith.kurak@gmail.com>
2023-09-26 09:02:31 -0400

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform, Button } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useNotifications, sendPushNotification } from '../../stores/notification-utils';
 
 export default function TabOneScreen() {
+  const { lastNotificationReceived, lastNotificationResponse, expoPushToken } = useNotifications();
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Notification test info</Text>
+      <Button title="Send notification" onPress={() => sendPushNotification(expoPushToken)} />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.subTitle}>Last notification received:</Text>
+      <Text style={styles.debugInfo}>{JSON.stringify(lastNotificationReceived)}</Text>
+      <Text style={styles.subTitle}>Last notification response:</Text>
+      <Text style={styles.debugInfo}>{JSON.stringify(lastNotificationResponse)}</Text>
     </View>
   );
 }
@@ -22,6 +27,19 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 16,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  debugInfo: {
+    width: '80%',
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 10,
+    color: 'black',
+    fontFamily: Platform.OS === 'ios' ? 'Courier New' : 'monospace',
   },
   separator: {
     marginVertical: 30,
